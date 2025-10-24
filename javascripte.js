fd1c2d6e06d3f11c5c2d394b5807acdd53c4ca70
Kraby440 <camille.cf222@gmail.com>
2025-10-24 16:15:43 +0200

    Add files via upload

---
+++ javascripte.js
@@ -0,0 +1,8 @@
+const clic_gris = document.getElementById("ft_gris");
+clic_gris.addEventListener("click", ()=>{document.body.style.backgroundImage = "url('images/nuit.webp')";});
+
+const clic_rose = document.getElementById("ft_rose");
+clic_rose.addEventListener("click", ()=>{document.body.style.backgroundImage = "url('images/fond_ecran.webp')";});
+
+const clic_vert = document.getElementById("ft_vert");
+clic_vert.addEventListener("click", ()=>{document.body.style.backgroundImage = "url('images/plaine.webp')";});
